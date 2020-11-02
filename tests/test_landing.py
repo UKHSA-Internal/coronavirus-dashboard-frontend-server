c3dd89233e691bb4bd19fd68670f316aadaeb1d7
Brogan <66849242+Kegois@users.noreply.github.com>
2020-11-02 11:30:15 +0000

    added rate calc tests for landing

--- tests/test_landing.py
+++ tests/test_landing.py
@@ -4,7 +4,7 @@ from flask import g
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
-from .utils import website_timestamp, timestamp, output_object_to_file
+from .utils import website_timestamp, timestamp, output_object_to_file, calculate_rate
 from app import app, inject_timestamps_tests
 
 
@@ -26,6 +26,30 @@ class TestLanding(unittest.TestCase):
                 # output_object_to_file("response.txt", response)
                 self.assertEqual(response.status_code, 200)
 
+    def test_cases_rate(self):
+        with inject_timestamps_tests(app, timestamp, website_timestamp):
+            with app.test_client() as c:
+                response = c.get('/')
+                data = response.data
+                test_rate = str(calculate_rate("newCasesBySpecimenDate")).encode('UTF-8')
+                assert test_rate in data
+
+    def test_deaths_rate(self):
+        with inject_timestamps_tests(app, timestamp, website_timestamp):
+            with app.test_client() as c:
+                response = c.get('/')
+                data = response.data
+                test_rate = str(calculate_rate("newDeaths28DaysByDeathDate")).encode('UTF-8')
+                assert test_rate in data
+    
+    def test_admissions_rate(self):
+        with inject_timestamps_tests(app, timestamp, website_timestamp):
+            with app.test_client() as c:
+                response = c.get('/')
+                data = response.data
+                test_rate = str(calculate_rate("newAdmissions")).encode('UTF-8')
+                print(test_rate)
+                assert test_rate in data
 
 if __name__ == "__main__":
     unittest.main()
--- tests/utils.py
+++ tests/utils.py
@@ -5,6 +5,7 @@ from flask import g
 import requests
 from datetime import datetime, timedelta, date
 import json
+import csv
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
@@ -13,7 +14,7 @@ load_dotenv(find_dotenv())
 from app import app, inject_timestamps_tests
 
 website_timestamp = requests.get('https://coronavirus.data.gov.uk/public/assets/dispatch/website_timestamp').content.decode('ascii')
-timestamp = "2020-10-29T15:39:48.6664795Z"
+timestamp = "2020-11-01T15:50:42.4511435Z"
 
 
 def output_object_to_file(filename: str, data: object, write_type: str = 'w') -> None:
@@ -27,12 +28,36 @@ def output_content_to_file(filename: str, data: object, write_type: str = 'w') -
     f.write(data.decode('UTF-8'))
     f.close()
 
-# def calculate_rate(metric= "newCasesBySpecimenDate", area="UK"):
-#     data = json.loads(requests.get(f'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=overview&structure=%7B%22{metric}%22:%22{metric}%22,%22date%22:%22date%22%7D').content.decode())
-#     latest_date = datetime.strptime(data["data"][0]["date"], "%Y-%m-%d")
-#     latest_complete_date = date.strftime(latest_date - timedelta(days=5), "%Y-%m-%d")
-#     if area == "UK":
-#         population = 66796800
-#     print(latest_complete_date)
+def calculate_rate(metric, area="UK"):
+    count = 0
+    start_date = False
+    data = json.loads(requests.get(f'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=overview&structure=%7B%22{metric}%22:%22{metric}%22,%22date%22:%22date%22%7D').content.decode())
+    latest_date = datetime.strptime(timestamp[:10], "%Y-%m-%d")
 
-# calculate_rate()
+    if metric == "newAdmissions":
+        latest_complete_date = date.strftime(latest_date - timedelta(days=4), "%Y-%m-%d")
+        latest_complete_date_week_prior = date.strftime(latest_date - timedelta(days=10), "%Y-%m-%d")
+    else:
+        latest_complete_date = date.strftime(latest_date - timedelta(days=5), "%Y-%m-%d")
+        latest_complete_date_week_prior = date.strftime(latest_date - timedelta(days=11), "%Y-%m-%d")
+    
+    if area == "UK":
+        population = 66796807
+    # else:
+    #     populations = csv.reader(open('popestimates.csv'))
+    #     for row in populations:
+    #         if row[0] == 'K02000001':
+    #             print (row[3])
+
+    
+    for item in data["data"]:
+        if item["date"] == latest_complete_date or start_date:
+            start_date = True
+            count += item[metric]
+        if item["date"] == latest_complete_date_week_prior:
+            break
+
+    rate = (count / population) * 100000
+    formatted = "{0:0.1f}".format(rate)
+    return formatted
+
