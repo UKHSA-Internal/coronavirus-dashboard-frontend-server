7d158a17e1eeee801f5ae10e26899dc9125d76ca
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2020-12-12 11:19:14 +0000

    Implement custom error handler

--- app/common/exceptions.py
+++ app/common/exceptions.py
@@ -5,27 +5,44 @@
 # Python:
 
 # 3rd party:
+from werkzeug.exceptions import HTTPException
+from flask import render_template, Response
 
 # Internal: 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 __all__ = [
-    'InvalidPostcode'
+    'InvalidPostcode',
+    'HandledException'
 ]
 
 
-class InvalidPostcode(Exception):
-    status_code = 400
+class HandledException(object):
+    template = None
+    template_kws = dict()
 
-    def __init__(self, postcode, status_code=None, payload=None):
-        Exception.__init__(self)
-        self.message = f'Invalid postcode: "{postcode}"'
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['message'] = self.message
-        return rv
+    def get_body(self, environ=None):
+        return render_template(self.template, **self.template_kws)
+
+
+class InvalidPostcode(HandledException, HTTPException):
+    code = 400
+    message_template = 'Invalid postcode: "{postcode}"'
+    template = "main.html"
+
+    def __init__(self, postcode):
+        self.postcode = postcode
+        self.template_kws = {
+            'invalid_postcode': True,
+            'message': self.message
+        }
+        super().__init__(description=self.message)
+
+    @property
+    def message(self):
+        return self.message_template.format(postcode=self.postcode)
+
