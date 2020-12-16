bd3edbed8665bed657086c9d2d44dd4a9a6d1345
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2020-12-16 13:51:05 +0000

    adds nhsTrust to order of smallest areas

--- app/postcode/views.py
+++ app/postcode/views.py
@@ -37,6 +37,7 @@ def get_by_smallest_areatype(items, areatype_getter):
         "ltla",
         "utla",
         "region",
+        "nhsTrust",
         "nhsRegion",
         "nation",
         "overview"
