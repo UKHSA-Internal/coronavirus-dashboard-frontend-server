b7cf85afaeee5bc1d3919676ae4b8df389c6ca86
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2020-12-17 15:49:32 +0000

    changes func signature from metric to category

--- app/postcode/utils.py
+++ app/postcode/utils.py
@@ -9,23 +9,25 @@ from typing import List, Dict, Union
 from functools import lru_cache
 
 # 3rd party:
+from flask import current_app as app
 
 # Internal:
 from ..common.caching import cache_client
 from ..common.data.queries import get_last_fortnight, change_by_metric
 from ..common.visualisation import plot_thumbnail, get_colour
+from ..common.data.variables import DestinationMetrics
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 postcode_pattern = re.compile(r'(^[a-z]{1,2}\d{1,2}[a-z]?\s?\d{1,2}[a-z]{1,2}$)', re.I)
 get_value = itemgetter("value")
 
-main_metric_names: List[str] = [
-    "newCasesByPublishDate",
-    "newDeaths28DaysByPublishDate",
-    "newAdmissions",
-    "newPCRTestsByPublishDate",
-]
+# main_metric_names: List[str] = [
+#     "newCasesByPublishDate",
+#     "newDeaths28DaysByPublishDate",
+#     "newAdmissions",
+#     "newPCRTestsByPublishDate",
+# ]
 
 
 @lru_cache(maxsize=256)
@@ -43,8 +45,10 @@ def get_validated_postcode(params: dict) -> Union[str, None]:
 
 
 @cache_client.memoize(60 * 60 * 6)
-def get_card_data(timestamp: str, metric_name: str, metric_data, graph=True, postcode=None):
-    change = change_by_metric(timestamp, metric_name, postcode)
+def get_card_data(timestamp: str, category: str, metric_data, graph=True, postcode=None):
+    metric_name = DestinationMetrics[category]["metric"]
+    change = change_by_metric(timestamp, category, postcode)
+    app.logger.warning(f"CHANGE =======> {timestamp} | {category} | {postcode} | {change}")
     colour = get_colour(change, metric_name)
 
     response = {
@@ -65,8 +69,9 @@ def get_fortnight_data(latest_timestamp: str,
                        area_name: str = "United Kingdom") -> Dict[str, dict]:
     result = dict()
 
-    for name in main_metric_names:
-        metric_data = get_last_fortnight(latest_timestamp, area_name, name)
-        result[name] = get_card_data(name, metric_data)
+    for category, metric_data in DestinationMetrics.items():
+        metric = DestinationMetrics[category]["metric"]
+        metric_data = get_last_fortnight(latest_timestamp, area_name, category)
+        result[metric] = get_card_data(latest_timestamp, category, metric_data)
 
     return result
