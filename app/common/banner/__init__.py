650d999507167921d131af3fdd1676dee64a3ece
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2020-12-24 14:17:01 +0000

    integrates banner

---
+++ app/common/banner/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin python3
+
+"""
+<Description of the programme>
+
+Author:        Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
+Created:       24 Dec 2020
+License:       MIT
+Contributors:  Pouria Hadjibagheri
+"""
+
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Python:
+
+# 3rd party:
+
+# Internal: 
+from .banner import *
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Header
+__author__ = "Pouria Hadjibagheri"
+__copyright__ = "Copyright (c) 2020, Public Health England"
+__license__ = "MIT"
+__version__ = "0.0.1"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
