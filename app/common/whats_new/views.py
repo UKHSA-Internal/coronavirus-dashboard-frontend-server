0fcfd7e2a84ba6b035a7aeeb6309f5742a85edbd
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-02-08 11:17:17 +0000

    implements changeLog banners

---
+++ app/common/whats_new/views.py
@@ -0,0 +1,49 @@
+#!/usr/bin python3
+
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Python:
+import re
+from json import loads
+from datetime import datetime
+
+# 3rd party:
+from markdown import markdown
+from flask import request, current_app as app
+
+# Internal:
+from ...storage import StorageClient
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+__all__ = [
+    'get_whats_new_banners'
+]
+
+
+BANNER_DATA = dict(
+    container="publicdata",
+    path="assets/cms/changeLog.json"
+)
+
+special_chars_pattern = re.compile(r"[\"')]")
+to_underscore_pattern = re.compile(r"[\s.(&,]+")
+
+
+def get_whats_new_banners(timestamp: str):
+    with StorageClient(**BANNER_DATA) as client:
+        full_data = loads(client.download().readall().decode())
+
+    if full_data is None:
+        full_data = dict()
+
+    data = full_data.get("changeLog", list())
+    datestamp = timestamp.split("T")[0]
+
+    for banner in filter(lambda b: b["date"] == datestamp, data):
+        app.logger.info(banner)
+        banner['anchor'] = special_chars_pattern.sub("", banner["headline"].lower())
+        banner['anchor'] = special_chars_pattern.sub("_", banner["anchor"])
+        banner['formatted_date'] = f"{datetime.strptime(banner['date'], '%Y-%m-%d'):%-d %B %Y}"
+
+        yield banner
