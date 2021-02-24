ea8d7f76318c3b336a0a7172ee06b8fe755e00af
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-02-24 15:07:27 +0000

    updates config for starlette

--- app/config.py
+++ app/config.py
@@ -3,13 +3,24 @@
 # Imports
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Python:
-from os import getenv
+from dataclasses import dataclass
+from os import getenv, path
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+dir_name = path.dirname(__file__)
+templates_dir = path.join(dir_name, "templates")
 
-class Config(object):
+
+@dataclass
+class Settings:
     TESTING = getenv("IS_DEV", "0") == "1"
     DEBUG = getenv("IS_DEV", "0") == "1"
+    template_path = templates_dir
+
+
+class Config(object):
+    TESTING = Settings.TESTING
+    DEBUG = Settings.DEBUG
     SECRET_KEY = getenv('FLASK_SECRET_KEY')
     APPLICATION_ROOT = "/"
