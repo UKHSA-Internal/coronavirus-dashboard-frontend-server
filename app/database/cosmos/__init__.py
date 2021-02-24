3480014860ccb261f279fb57d5572233081def72
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-02-24 15:03:31 +0000

    moves cosmos settings to dedicate dir

---
+++ app/database/cosmos/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin python3
+
+"""
+<Description of the programme>
+
+Author:        Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
+Created:       23 Aug 2020
+License:       MIT
+Contributors:  Pouria Hadjibagheri
+"""
+
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Python:
+
+# 3rd party:
+
+# Internal: 
+from .database import CosmosDB
+from .utils import Collection
+# from .models import BaseModel
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Header
+__author__ = "Pouria Hadjibagheri"
+__copyright__ = "Copyright (c) 2020, Public Health England"
+__license__ = "MIT"
+__version__ = "0.0.1"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
