fa9d92fc856490001a0216ccf65b2c5bef652451
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-02-28 12:47:08 +0000

    switches to generic template renderer

--- app/landing/views.py
+++ app/landing/views.py
@@ -16,8 +16,6 @@ from datetime import datetime
 from os.path import abspath, split as split_path, join as join_path
 
 # 3rd party:
-from starlette.templating import Jinja2Templates
-
 from pandas import DataFrame
 
 # Internal:
@@ -25,16 +23,15 @@ from ..common.data.variables import DestinationMetrics, IsImproving
 from ..common.utils import get_release_timestamp
 from .graphics import get_timeseries
 from ..database.postgres import Connection
-from ..config import Settings
+from ..template_processor import render_template
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 __all__ = [
-    'home_page'
+    'home_page',
+    'get_home_page'
 ]
 
-
-templates = Jinja2Templates(directory=Settings.template_path)
 
 curr_dir, _ = split_path(abspath(__file__))
 queries_dir = join_path(curr_dir, "queries")
@@ -90,7 +87,8 @@ async def get_landing_data(conn, timestamp):
         await values,
         columns=["areaCode", "areaType", "areaName", "date", "metric", "value", "rank"]
     )
-    df.date = df.date.map(lambda x: f"{x:%-d %B %Y}")
+
+    df = df.assign(formatted_date=df.date.map(lambda x: f"{x:%-d %B %Y}"))
 
     return df
 
@@ -106,63 +104,30 @@ def is_improving(metric, value):
     return None
 
 
-def get_data(metric, data):
-    float_metrics = ["Rate", "Percent"]
-
-    try:
-        value = data.loc[data.metric == metric, ["value", "date", "areaName", "areaType"]].values[0]
-    except IndexError:
-        return None
-
-    result = {
-        "date": value[1],
-        "areaName": value[2],
-        "areaType": value[3],
-    }
-
-    is_float = any(m in metric for m in float_metrics)
-
-    try:
-        float_val = float(value[0])
-        result["raw"] = float_val
-
-        if (int_val := int(float_val)) == float_val and not is_float:
-            result["value"] = format(int_val, ",d")
-            return result
-
-        result["value"] = format(float_val, ".1f")
-        return result
-    except ValueError:
-        pass
-
-    result["raw"] = value[0]
-    result["value"] = value[0]
-
-    return result
-
-
-templates.env.filters['get_data'] = get_data
-
-
-async def home_page(request):
-    timestamp = await get_release_timestamp()
-
+async def get_home_page(request, timestamp: str, invalid_postcode=None) -> render_template:
     async with Connection() as conn:
         data = await get_landing_data(conn, timestamp)
+
         time_series = {
             key: await value
             async for key, value in get_timeseries(conn, timestamp)
         }
 
-    return templates.TemplateResponse(
+    return await render_template(
+        request,
         "main.html",
         context={
-            "request": request,
             "timestamp": timestamp,
             "data": data,
             "cards": DestinationMetrics,
             "plots": time_series,
             "is_improving": is_improving,
-            "DEBUG": Settings.DEBUG
+            "invalid_postcode": invalid_postcode
         }
     )
+
+
+async def home_page(request) -> render_template:
+    timestamp = await get_release_timestamp()
+
+    return await get_home_page(request, timestamp)
