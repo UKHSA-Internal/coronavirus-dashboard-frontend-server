e1ee742d1d9e6635753ce64c48fef9d5f197cd33
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-02-28 12:46:21 +0000

    switches to async implementation

--- app/common/whats_new/views.py
+++ app/common/whats_new/views.py
@@ -8,11 +8,9 @@ from json import loads
 from datetime import datetime
 
 # 3rd party:
-from markdown import markdown
-from flask import request, current_app as app
 
 # Internal:
-from ...storage import StorageClient
+from ...storage import AsyncStorageClient
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -30,9 +28,12 @@ special_chars_pattern = re.compile(r"[\"')]")
 to_underscore_pattern = re.compile(r"[\s.(&,]+")
 
 
-def get_whats_new_banners(timestamp: str):
-    with StorageClient(**BANNER_DATA) as client:
-        full_data = loads(client.download().readall().decode())
+async def get_whats_new_banners(timestamp: str):
+    async with AsyncStorageClient(**BANNER_DATA) as client:
+        data_io = await client.download()
+        raw_data = await data_io.readall()
+
+    full_data = loads(raw_data.decode())
 
     if full_data is None:
         full_data = dict()
@@ -45,9 +46,12 @@ def get_whats_new_banners(timestamp: str):
         data
     )
 
+    results = list()
+
     for banner in filtered_data:
         banner['anchor'] = special_chars_pattern.sub("", banner["headline"].lower())
         banner['anchor'] = to_underscore_pattern.sub("_", banner["anchor"])
         banner['formatted_date'] = f"{datetime.strptime(banner['date'], '%Y-%m-%d'):%-d %B %Y}"
+        results.append(banner)
 
-        yield banner
+    return results
