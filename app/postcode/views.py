cd30264a383a8691f1a49e95268c0b5da7747c42
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-04-25 20:14:28 +0100

    breaks down queries and integrates Redis in postcodes

--- app/postcode/views.py
+++ app/postcode/views.py
@@ -4,24 +4,30 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Python:
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 from os.path import abspath, split as split_path, join as join_path
 from operator import itemgetter
 from json import load
-from typing import Union, Any
+from typing import Union
 from asyncio import gather
+from hashlib import blake2b
+from io import BytesIO
+from random import randint
+from functools import wraps
 
 # 3rd party:
 
-from pandas import DataFrame
+from pandas import DataFrame, concat, read_pickle
 
 # Internal:
 from .types import QueryDataType
 from .utils import get_validated_postcode
-from ..common.utils import get_release_timestamp
-from ..common.data.variables import DestinationMetrics, IsImproving
-from ..database.postgres import Connection
-from ..template_processor import render_template
+from app.common.utils import get_release_timestamp
+from app.common.data.variables import DestinationMetrics, IsImproving
+from app.database.postgres import Connection
+from app.template_processor import render_template
+from app.context.redis import get_redis_pool, shutdown_redis_pool
+from app.middleware.tracers.utils import trace_async_method_operation
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -45,46 +51,158 @@ with open(join_path(assets_dir, "query_params.json")) as fp:
     query_data: QueryDataType = load(fp)
 
 
-async def get_postcode_data(timestamp: str, postcode: str) -> DataFrame:
-    ts = datetime.fromisoformat(timestamp.replace("5Z", ""))
-    partition_ts = f"{ts:%Y_%-m_%-d}"
-    msoa_partition = f"{partition_ts}_msoa"
-    msoa_metric = query_data["local_data"]["msoa_metric"]
+with open(join_path(queries_dir, "single_query.sql")) as fp:
+    single_query = fp.read()
 
-    query = local_data_query.format(
-        msoa_partition=msoa_partition,
-        partition_date=partition_ts
+
+with open(join_path(queries_dir, "single_query_msoa.sql")) as fp:
+    msoa_query = fp.read()
+
+
+with open(join_path(queries_dir, "locations.sql")) as fp:
+    locations_query = fp.read()
+
+
+class Redis:
+    _name = "Redis"
+
+    def __init__(self):
+        self._conn = get_redis_pool()
+        if isinstance(self._conn.address, (tuple, list)):
+            self.account_name = self._conn.address[0].split(".")[0]
+            self.url = str.join(":", self._conn.address)
+        else:
+            self.account_name = self._conn.address.split(".")[0]
+            self.url = self._conn.address
+
+    @trace_async_method_operation(
+        "url",
+        name="account_name",
+        dep_type="_name",
+        action="GET"
     )
+    async def get(self, key):
+        return await self._conn.get(key)
 
-    substitutes = (
-        query_data["local_data"]["metrics"],
-        postcode,
-        f"{msoa_metric}%",
-        ["%Percentage%", "%Rate%"]
+    @trace_async_method_operation(
+        "url",
+        name="account_name",
+        dep_type="_name",
+        action="SET"
     )
+    async def set(self, key, value, expire=None):
+        return await self._conn.set(key, value, expire=expire)
 
-    # log_query = query
-    #
-    # for index, sub in enumerate(substitutes, start=1):
-    #     if not isinstance(sub, list):
-    #         log_query = log_query.replace(f"${index}", f"'{sub}'")
-    #     else:
-    #         log_query = log_query.replace(f"${index}", f"'{{{str.join(',', sub)}}}'")
-    #
-    # print(log_query)
 
-    queries = query.split(";")
-    async with Connection() as conn, conn.transaction():
-        for qr in queries[:-2]:
-            await conn.execute(qr, *substitutes[:2])
-        result = await conn.fetch(queries[-2], *substitutes[2:])
-        await conn.execute("DROP TABLE data; DROP TABLE msoa;")
+def from_cache_or_db(prefix):
+    def outer(func):
+        @wraps(func)
+        async def inner(*args, **kwargs):
+            redis = Redis()
 
-    df = DataFrame(result, columns=query_data["local_data"]["column_names"])
+            raw_key = prefix + str.join("|", map(str, [*args, *kwargs.values()]))
+            cache_key = blake2b(raw_key.encode(), digest_size=6).hexdigest()
 
-    df = df.assign(formatted_date=df.date.map(lambda x: f"{x:%-d %B %Y}"))
+            buffer = BytesIO()
+            if (redis_result := await redis.get(cache_key)) is not None:
+                buffer.write(redis_result)
+                buffer.seek(0)
+                result = read_pickle(buffer)
+                return result
+
+            result: DataFrame = await func(*args, **kwargs)
+            result.to_pickle(buffer)
+            buffer.seek(0)
+            await redis.set(cache_key, buffer.read(), randint(30, 300) * 60)
+            return result
+
+        return inner
+    return outer
+
+
+@from_cache_or_db("FRONTEND::PC::")
+async def get_data(partition_name, area_type, area_id, timestamp):
+    numeric_metrics = ["%Percentage%", "%Rate%"]
+    local_metrics = query_data["local_data"]["metrics"]
+    msoa_metric = [f'{query_data["local_data"]["msoa_metric"]}%']
+
+    if area_type == "msoa":
+        query = msoa_query
+        args = [
+            area_id,
+            local_metrics,
+            msoa_metric
+        ]
+    else:
+        query = single_query
+        args = [
+            numeric_metrics,
+            area_id,
+            local_metrics
+        ]
+
+    query = query.format(partition_id=f"{timestamp}_{partition_name}")
+
+    async with Connection() as conn:
+        result = await conn.fetch(query, *args)
+
+    df = DataFrame(
+        result,
+        columns=query_data["local_data"]["column_names"],
+    ).astype(query_data["local_data"]["column_types"])
 
     return df
+
+
+async def get_postcode_data(timestamp: str, postcode: str) -> DataFrame:
+    msoa_metric = query_data["local_data"]["msoa_metric"]
+    ts = datetime.fromisoformat(timestamp.replace("5Z", ""))
+    partition_ts = f"{ts:%Y_%-m_%-d}"
+    partition_names = {
+        "overview": "other",
+        "nation": "other",
+        "region": "other",
+        "nhsRegion": "other",
+        "utla": "utla",
+        "ltla": "ltla",
+        "nhsTrust": "nhstrust",
+        "msoa": "msoa",
+    }
+
+    async with Connection() as conn:
+        area_codes = await conn.fetch(locations_query, postcode)
+
+    if not len(area_codes):
+        return DataFrame()
+
+    tasks = list()
+    for area_data in area_codes:
+        area_type = area_data["area_type"]
+        task = get_data(
+            partition_names[area_type],
+            area_type,
+            area_data["id"],
+            partition_ts
+        )
+
+        tasks.append(task)
+
+    data = await gather(*tasks)
+
+    result = concat(data).reset_index(drop=True)
+    result["rank"] = result.groupby("metric")[["priority", "date"]].rank(ascending=True)
+    filters = (
+            (result["rank"] == 1) |
+            (result["metric"].str.startswith(msoa_metric))
+    )
+    result = result.loc[filters, :]
+
+    result = result.assign(
+        formatted_date=result.date.map(lambda x: f"{x:%-d %B %Y}"),
+        postcode=area_codes[0]['postcode']
+    )
+
+    return result
 
 
 def is_improving(metric: str, value: Union[float, int]) -> Union[bool, None]:
@@ -99,8 +217,7 @@ def is_improving(metric: str, value: Union[float, int]) -> Union[bool, None]:
 
 
 def get_area_data(df: DataFrame) -> DataFrame:
-    small_areas = df.sort_values("rank").iloc[0]
-
+    small_areas = df.sort_values("priority").iloc[0]
     return small_areas
 
 
@@ -136,3 +253,4 @@ async def postcode_page(request) -> render_template:
             "is_improving": is_improving
         }
     )
+
