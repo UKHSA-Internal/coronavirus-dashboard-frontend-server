46b1c752cf911cadc419c3304d8b96e68e9cba70
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-04-30 14:39:09 +0100

    general improvements

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -14,12 +14,12 @@ fi
 # Start Supervisor, with Nginx and ASGI
 #exec /usr/bin/supervisord -c /opt/supervisor/supervisord.conf
 exec uvicorn app.main:app --uds /opt/uvicorn.sock \
-                          --workers 2 \
+                          --workers 1 \
                           --loop uvloop \
                           --proxy-headers \
                           --host 0.0.0.0 \
                           --port 5100 \
-                          --backlog 128 \
-                          --timeout-keep-alive 10 \
+                          --backlog 256 \
+                          --timeout-keep-alive 20 \
                           --limit-max-requests 64 \
                           --http httptools
