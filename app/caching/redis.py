7c51d006cd5ef30a3a05d6545ee2a8bc1c320b37
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-04-30 01:35:18 +0100

    changes caching lib

--- app/caching/redis.py
+++ app/caching/redis.py
@@ -8,6 +8,7 @@ from ssl import SSLContext, CERT_REQUIRED, PROTOCOL_TLSv1_2
 # 3rd party:
 import certifi
 from aioredis import create_redis_pool
+from aredis import StrictRedis
 
 # Internal: 
 from app.middleware.tracers.utils import trace_async_method_operation
@@ -29,11 +30,21 @@ class Redis:
     _name = "Redis"
 
     def __init__(self, raw_key=None):
-        self._connection = create_redis_pool(
-            **Settings.redis,
-            minsize=5,
-            ssl=ssl_context,
-            timeout=10
+        # self._connection = create_redis_pool(
+        #     **Settings.redis,
+        #     minsize=5,
+        #     ssl=ssl_context,
+        #     timeout=10
+        # )
+        self._conn = StrictRedis(
+            host=Settings.redis["address"][0],
+            port=Settings.redis["address"][1],
+            password=Settings.redis["password"],
+            ssl=True,
+            ssl_context=ssl_context,
+            db=0,
+            connect_timeout=2,
+            retry_on_timeout=True
         )
 
         address = Settings.redis['address']
@@ -46,16 +57,18 @@ class Redis:
 
         self.key = raw_key
 
-    def __await__(self):
-        yield from self._connection.__await__()
+    # def __await__(self):
+    #     yield from self._connection.__await__()
 
     async def __aenter__(self):
-        self._conn = await self._connection
+        # self._conn = await self._conn
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        self._conn.close()
-        await self._conn.wait_closed()
+        pass
+        # self._conn.
+        # self._conn.close()
+        # await self._conn.wait_closed()
 
     @trace_async_method_operation(
         "url", "key",
@@ -73,7 +86,7 @@ class Redis:
         action="SET"
     )
     async def set(self, key, value, expire=None):
-        return await self._conn.set(key, value, expire=expire)
+        return await self._conn.set(key, value, ex=expire)
 
     @trace_async_method_operation(
         "url",
