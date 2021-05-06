db06566fed4f09ee0d0409dcd18e24b707261333
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-05-06 11:10:06 +0100

    general improvements

--- server/gunicorn_conf.py
+++ server/gunicorn_conf.py
@@ -30,43 +30,26 @@ __license__ = "MIT"
 __version__ = "0.0.1"
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-workers_per_core_str = getenv("WORKERS_PER_CORE", "1")
+workers_per_core_str = getenv("WORKERS_PER_CORE", "2")
 max_workers_str = getenv("MAX_WORKERS")
 use_max_workers = None
 
 if max_workers_str:
     use_max_workers = int(max_workers_str)
 
-web_concurrency_str = getenv("WEB_CONCURRENCY", None)
-
 host = getenv("HOST", "0.0.0.0")
 port = getenv("GUNICORN_PORT", "5100")
 
 use_bind = getenv("BIND", f"{host}:{port}")
-# use_loglevel = getenv("LOG_LEVEL", "INFO")
-
 
 cores = cpu_count()
 workers_per_core = float(workers_per_core_str)
 default_web_concurrency = workers_per_core * cores
 
-
-# if web_concurrency_str:
-#     web_concurrency = int(web_concurrency_str)
-#     assert web_concurrency > 0
-# else:
 web_concurrency = max(int(default_web_concurrency), 2)
 
-    # if use_max_workers:
-    #     web_concurrency = min(web_concurrency, use_max_workers)
-
-
-# accesslog_var = getenv("ACCESS_LOG", "-")
-# use_accesslog = accesslog_var or None
-# errorlog_var = getenv("ERROR_LOG", "-")
-# use_errorlog = errorlog_var or None
 graceful_timeout_str = getenv("GRACEFUL_TIMEOUT", "8")
-timeout_str = getenv("TIMEOUT", "20")
+timeout_str = getenv("TIMEOUT", "30")
 keepalive_str = getenv("KEEP_ALIVE", "10")
 
 from sys import stdout, stderr
