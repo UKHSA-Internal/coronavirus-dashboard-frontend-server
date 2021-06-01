5d5a53e89ec76430b9e6c8da082231edd08abc0a
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-06-01 09:59:02 +0100

    takes process manager out of circulation

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -2,4 +2,13 @@
 set -e
 
 /usr/sbin/sshd
-exec gunicorn -k app.uvicorn_worker.ServerUvicornWorker -c /opt/gunicorn_conf.py app.main:app
+exec uvicorn app.main:app --workers 8                 \
+                          --host 0.0.0.0              \
+                          --loop uvloop               \
+                          --port 5100                 \
+                          --proxy-headers             \
+                          --timeout-keep-alive 5      \
+                          --backlog 128               \
+                          --limit-concurrency 16
+
+#exec gunicorn -k app.uvicorn_worker.ServerUvicornWorker -c /opt/gunicorn_conf.py app.main:app
