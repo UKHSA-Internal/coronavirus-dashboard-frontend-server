fbd0f783b62fba2e50c54ec888311d615502405e
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-10-20 12:42:30 +0100

    updates params in postcode queries

--- app/postcode/queries/locations.sql
+++ app/postcode/queries/locations.sql
@@ -1,4 +1,5 @@
-SELECT id, area_type, postcode
+SELECT id, ref.area_type, area_name, postcode, priority
 FROM covid19.postcode_lookup
     JOIN covid19.area_reference AS ref ON ref.id = area_id
+    JOIN covid19.area_priorities AS ap ON ap.area_type = ref.area_type
 WHERE UPPER(REPLACE(postcode, ' ', '')) = $1;
