e5cb102e0ec63539633d252a1dbc02306f245839
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-10-23 13:01:33 +0100

    implements get_all method

--- app/caching/redis.py
+++ app/caching/redis.py
@@ -3,7 +3,7 @@
 # Imports
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Python:
-from typing import NoReturn
+from typing import NoReturn, Union, List
 from inspect import signature
 from hashlib import blake2b
 from functools import wraps
@@ -75,7 +75,16 @@ class Redis:
         return await self._conn.get(key)
 
     @trace_async_method_operation(
-        "url", "_key",
+        "url", "_keys",
+        name="account_name",
+        dep_type="_name",
+        action="MGET"
+    )
+    async def get_all(self, keys):
+        return await self._conn.mget(*keys)
+
+    @trace_async_method_operation(
+        "url", "field", "_key",
         name="account_name",
         dep_type="_name",
         action="HGET"
@@ -87,10 +96,19 @@ class Redis:
         "url", "_key",
         name="account_name",
         dep_type="_name",
-        action="SET"
+        action="SETNX"
     )
     async def set(self, key, value, expire=None):
-        return await self._conn.set(key, value, expire=expire)
+        return await self._conn.setnx(key, value, expire=expire)
+
+    @trace_async_method_operation(
+        "url", "_key",
+        name="account_name",
+        dep_type="_name",
+        action="SETEX NX"
+    )
+    async def set(self, key, value, expire=None):
+        return await self._conn.set(key, value, expire=expire, exist='SET_IF_NOT_EXIST')
 
     @trace_async_method_operation(
         "url", "_key",
@@ -206,23 +224,69 @@ class FromCacheOrDBBase:
 
         return self._execute
 
-    def cache_key(self, bound_inputs) -> str:
+    def cache_key(self, bound_inputs, area_id, area_type) -> str:
         raise NotImplementedError()
 
     async def _execute(self, *args, **kwargs):
         bound_inputs = self.sig.bind(*args, **kwargs)
         request = bound_inputs.arguments.pop("request")
-        cache_key = self.cache_key(bound_inputs)
+        try:
+            area_id = bound_inputs.arguments.pop("area_id")
+            area_type = bound_inputs.arguments.pop("area_type")
+        except KeyError:
+            area_id = None
+            area_type = None
+
+        if area_id is None:
+            cache_key = self.cache_key(bound_inputs, area_id=area_id, area_type=area_type)
+        elif isinstance(area_id, str):
+            cache_key = self.cache_key(bound_inputs, area_id=area_id, area_type=area_type)
+        else:
+            cache_key = [
+                self.cache_key(bound_inputs, area_id=aid, area_type=atype)
+                for aid, atype in zip(area_id, area_type)
+            ]
 
         async with Redis(request, cache_key) as redis:
-            results = await self._from_cache(redis, cache_key)
+            results: Union[DataFrame, bytes] = await self._from_cache(redis, cache_key)
 
-            if results is not None:
+            if isinstance(cache_key, (str, bytes)) and results is not None:
                 return self.process_cache_results(results)
+            elif hasattr(cache_key, "__iter__"):
+                for index, (key, res) in enumerate(zip(cache_key, results)):
+                    if res is not None:
+                        results[index] = self.process_cache_results(res)
+                        continue
 
-            results = await self.func(request, *bound_inputs.args, **bound_inputs.kwargs)
-            db_results = self.process_db_results(results)
-            await self._cache_results(redis, cache_key, db_results)
+                    db_res = await self.func(
+                        request,
+                        *bound_inputs.args,
+                        area_id=area_id,
+                        area_type=area_type,
+                        **bound_inputs.kwargs
+                    )
+
+                    results[index] = self.process_db_results(db_res)
+
+                    await self._cache_results(redis, key, results[index])
+            elif isinstance(cache_key, str):
+                results = await self.func(
+                    request,
+                    *bound_inputs.args,
+                    area_id=area_id,
+                    area_type=area_type,
+                    **bound_inputs.kwargs
+                )
+                db_results = self.process_db_results(results)
+                await self._cache_results(redis, cache_key, db_results)
+            elif area_id is None:
+                results = await self.func(
+                    request,
+                    *bound_inputs.args,
+                    **bound_inputs.kwargs
+                )
+                db_results = self.process_db_results(results)
+                await self._cache_results(redis, cache_key, db_results)
 
         return results
 
@@ -232,8 +296,11 @@ class FromCacheOrDBBase:
     def process_cache_results(self, results: bytes) -> DataFrame:
         raise NotImplementedError()
 
-    async def _from_cache(self, redis, cache_key: str) -> bytes:
-        return await redis.get(cache_key)
+    async def _from_cache(self, redis, cache_key: Union[str, List[str]]) -> Union[bytes, List[bytes]]:
+        if isinstance(cache_key, str):
+            return await redis.get(cache_key)
+
+        return await redis.get_all(cache_key)
 
     async def _cache_results(self, redis, cache_key: str, results: bytes) -> NoReturn:
         await redis.set(
@@ -247,7 +314,7 @@ class FromCacheOrDB(FromCacheOrDBBase):
     def __init__(self, prefix, ttl=None):
         super().__init__(prefix, ttl=ttl or LONG_TERM_CACHE_TTL)
 
-    def cache_key(self, bound_inputs) -> str:
+    def cache_key(self, bound_inputs, area_id=None, area_type=None) -> str:
         key = [*bound_inputs.args, *bound_inputs.kwargs.values()]
         raw_key = str.join("|", map(str, key))
         return raw_key
@@ -270,20 +337,24 @@ class FromCacheOrDB(FromCacheOrDBBase):
 
 
 class FromCacheOrDBMainData(FromCacheOrDBBase):
-    def cache_key(self, bound_inputs) -> str:
-        try:
-            area_id = bound_inputs.arguments["area_id"]
-            if bound_inputs.arguments["area_type"] == "overview":
+    def cache_key(self, bound_inputs, area_id, area_type) -> str:
+        if area_type is not None:
+            if area_type == "overview":
                 area_id = "UK"
             timestamp = datetime.strptime(bound_inputs.arguments["timestamp"], "%Y_%m_%d")
             raw_key = f"{self.prefix}-{timestamp:%Y-%m-%d}-{area_id}"
-        except KeyError:
+        else:
             key = [*bound_inputs.args, *bound_inputs.kwargs.values()]
             raw_key = f'SUMMARY::{str.join("|", map(str, key))}'
 
         return raw_key
 
     def process_db_results(self, results: DataFrame) -> bytes:
+        try:
+            results.date = results.date.map(lambda x: f"{x:%Y-%m-%d}")
+        except ValueError:
+            pass
+
         res = (
             results
             .rename(columns={
